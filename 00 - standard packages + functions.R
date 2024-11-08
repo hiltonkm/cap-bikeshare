1375d1808dd60ca4de653d6374aa6e8bdb2b7ee0
hiltonkm <katherine.mae.hilton@gmail.com>
2024-11-08 16:46:55 -0500

    adding in comments for function

--- 00 - standard packages + functions.R
+++ 00 - standard packages + functions.R
@@ -28,15 +28,24 @@ library(viridis)
 library(bigrquery)
 library(DBI)
 
+# FUNCTIONS --------------------------------------------------------------------
+## NOTE: This is the function that will be used to bind all the individual 
+## bikeshare data sets together AND upload those individual datasets to BQ
 cap_bikeshare_function <- function(x) {
+  ## Print filename
   print(x)
+  ## Read in CSV
   df <- read_csv(file.path(output_wd, "unzipped", x), col_types = "cccccccccccccc") %>% 
     mutate(file=x)
+  ## Print # of columns
   print(ncol(df))
+  ## Create a tablename without -'s and .csv
   table_name <- str_replace_all(x, "-", "_")
   table_name <- str_replace(table_name, "_tripdata.csv", "")
+  ## Upload raw csv's as tables read in above to raw bikeshare dataset in Bq
   bq_table_create(paste0("cap-bikeshare-441121.raw_bikeshare_data.",table_name), as_bq_fields(df))
   bq_table_upload(paste0("cap-bikeshare-441121.raw_bikeshare_data.",table_name), df)
   gc()
+  ## Return final df to be bound together with other files
   return(df)
 }
